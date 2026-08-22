8cfc30cf54c618920324cd89cd2b1954c9e0fcbe
oluna2078 <oluna2078@protonmail.com>
2026-08-22 18:42:26 +0200

    tidied up the code

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+# --INPUT--
+# list of addends
 sums: list[float] = [45.87, 67.88, 67.96, 67.02, 128.89, 200.74, 140.25, 69.69, 100.37, 40.7, 500.59, 30.45, 45.89]
-
+# sum of some addends the program solves for
 partial_sum: float = 421.24
-total_sum: float = 1506.3
+# number of categories used for addend comparison
+CATEGORY_RES: int = 5
 
 
-CATEGORY_RES: int = 10
+# calculate total sum
+#total_sum: float = 0
+#for i in sums:
+#    total_sum = total_sum + i
+#total_sum = round(total_sum, 2)
+
+
 categorised_sums: list[list[float]] = []
 
 
@@ -23,19 +32,15 @@ for i in range(CATEGORY_RES):
     categorised_sums.append([])
     for j in sums:
         if j <= cat_limit:
-            print(f"++ {j}")
+            print(j)
             categorised_sums[i].append(j)
         else:
             rest.append(j)
     sums = rest
-    print(f"--------------------- CAT_LIMIT_{i+1}: {cat_limit}")
-    #print(rest)
+    print(f"------- CAT_LIMIT_{CATEGORY_RES - i}: {cat_limit}")
     rest = []
     
 
 
-
-
-print(sums)
 print(categorised_sums)
 
