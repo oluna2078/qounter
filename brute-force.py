b16d03c68aa48ed54652576208c8f4acd4ec15f9
oluna2078 <oluna2078@protonmail.com>
2026-08-26 01:42:43 +0200

    brute-force now implemented with arbitrary recursion depth

--- brute-force.py
+++ brute-force.py
@@ -3,117 +3,41 @@
 sums: list[float] = [45.87, 67.58, 67.96, 67.02, 128.89, 200.74, 140.25, 69.69, 100.37, 40.7, 500.59, 30.45, 45.89, 45.11, 67.88, 67.99, 67.20, 124.77, 240.74, 600.25, 69.69, 111.37, 4011.7, 510.59, 10.45, 45.29, 5.66, 3.93, 2.1, 75.09, 7.04]
 # sum of some addends the program solves for
 partial_sum: float = 600.25
+REC_DEPTH: int = len(sums)
 
 # remove all bigger than partial_sum
 new_sums = sums.copy()
 for i in sums:
     if i > partial_sum:
         new_sums.remove(i)
-    elif i == partial_sum:
-        print(f"FOUND SOLUTION: {i} = {i}")
 sums = new_sums
 
 
-
-# 2 addends
-sums_1 = sums.copy()
-for i in sums:
-    sums_1.pop(0)
-    for j in sums_1:
-        test_sum = round(i + j, 2)
-        print(f"{i} + {j}")
-        #print(sums_1)
-        if test_sum == partial_sum:
-            print(f"FOUND SOLUTION: {i} + {j} = {test_sum}")
-
-
-# 3 addends
-sums_1 = sums.copy()
-for i in sums:
-    sums_1.pop(0)
-    sums_2 = sums_1.copy()
-    for j in sums_1:
-        sums_2.pop(0)
-        for h in sums_2:
-            test_sum = round(i + j + h, 2)
-            print(f"{i} + {j} + {h}")
-            #print(sums_1)
-            if test_sum == partial_sum:
-                print(f"FOUND SOLUTION: {i} + {j} + {h} = {test_sum}")
+def rec_sum_search_helper(
+    rec_depth: int,
+    list: list[float],
+    key: float,
+    past_sum: float,
+    visual: str
+):
+    if rec_depth <= 0:
+        for i in list:
+            test_sum = round(past_sum + i, 2)
+            #print(f"{visual}{i} = {test_sum}")
+            if test_sum == key:
+                print(f"FOUND SOLUTION: {visual}{i} = {test_sum}")
+    else:
+        list_cut = list.copy()
+        for i in list:
+            past_sum_new = round(past_sum + list_cut.pop(0), 2)
+            visual_new = f"{visual}{i} + "
+            rec_sum_search_helper(rec_depth - 1, list_cut, key, past_sum_new, visual_new)
 
 
-# 4 addends
-sums_1 = sums.copy()
-for i in sums:
-    sums_1.pop(0)
-    sums_2 = sums_1.copy()
-
-    for j in sums_1:
-        sums_2.pop(0)
-        sums_3 = sums_2.copy()
-
-        for h in sums_2:
-            sums_3.pop(0)
-
-            for g in sums_3:
-                test_sum = round(i + j + h + g, 2)
-                print(f"{i} + {j} + {h} + {g} = {test_sum}")
-                #print(sums_1)
-                if test_sum == partial_sum:
-                    print(f"FOUND SOLUTION: {i} + {j} + {h} + {g} = {test_sum}")
+def rec_sum_search(rec_depth: int, list: list[float], key: float):
+    rec_sum_search_helper(rec_depth, list, key, 0, "")
 
 
-# 5 addends
-sums_1 = sums.copy()
-for i in sums:
-    sums_1.pop(0)
-    sums_2 = sums_1.copy()
-
-    for j in sums_1:
-        sums_2.pop(0)
-        sums_3 = sums_2.copy()
-
-        for h in sums_2:
-            sums_3.pop(0)
-            sums_4 = sums_3.copy()
-
-            for g in sums_3:
-                sums_4.pop(0)
-
-                for f in sums_4:
-                    test_sum = round(i + j + h + g + f, 2)
-                    print(f"{i} + {j} + {h} + {g} + {f} = {test_sum}")
-                    #print(sums_1)
-                    if test_sum == partial_sum:
-                        print(f"FOUND SOLUTION: {i} + {j} + {h} + {g} + {f} = {test_sum}")
-
-
-# 6 addends
-sums_1 = sums.copy()
-for i in sums:
-    sums_1.pop(0)
-    sums_2 = sums_1.copy()
-
-    for j in sums_1:
-        sums_2.pop(0)
-        sums_3 = sums_2.copy()
-
-        for h in sums_2:
-            sums_3.pop(0)
-            sums_4 = sums_3.copy()
-
-            for g in sums_3:
-                sums_4.pop(0)
-                sums_5 = sums_4.copy()
-
-                for f in sums_4:
-                    sums_5.pop(0)
-
-                    for e in sums_5:
-                        test_sum = round(i + j + h + g + f + e, 2)
-                        print(f"{i} + {j} + {h} + {g} + {f} + {e} = {test_sum}")
-                        #print(sums_1)
-                        if test_sum == partial_sum:
-                            print(f"FOUND SOLUTION: {i} + {j} + {h} + {g} + {f} + {e} = {test_sum}")
-
+for i in range(REC_DEPTH + 1):
+    rec_sum_search(i, sums, partial_sum)
 
