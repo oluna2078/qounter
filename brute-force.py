aa0ab63f0371f79112a924dfe48c5c718b699150
oluna2078 <oluna2078@protonmail.com>
2026-08-26 01:49:25 +0200

    used example numbers from .ods

--- brute-force.py
+++ brute-force.py
@@ -1,8 +1,8 @@
 # --INPUT--
 # list of addends
-sums: list[float] = [45.87, 67.58, 67.96, 67.02, 128.89, 200.74, 140.25, 69.69, 100.37, 40.7, 500.59, 30.45, 45.89, 45.11, 67.88, 67.99, 67.20, 124.77, 240.74, 600.25, 69.69, 111.37, 4011.7, 510.59, 10.45, 45.29, 5.66, 3.93, 2.1, 75.09, 7.04]
+sums: list[float] = [45.87, 67.88, 67.96, 67.02, 128.89, 200.74, 140.25, 69.69, 100.37, 40.7, 500.59, 30.45, 45.89]
 # sum of some addends the program solves for
-partial_sum: float = 600.25
+partial_sum: float = 984.44
 REC_DEPTH: int = len(sums)
 
 # remove all bigger than partial_sum
