d73d15d30ede17dbb2289c43650d654a67d4e5f0
oluna2078 <oluna2078@protonmail.com>
2026-08-31 23:01:43 +0200

    updated gui

--- qounter-gui.py
+++ qounter-gui.py
@@ -14,97 +14,129 @@ class App:
         self.filepath: str = ""
 
         root.geometry('1000x500')
+        root.config(bg='lightcyan3')
 
         root.rowconfigure(0, weight=1)
-        root.rowconfigure(1, weight=3)
-        root.rowconfigure(2, weight=3)
-        root.rowconfigure(3, weight=1)
-        root.rowconfigure(4, weight=15)
-        root.rowconfigure(5, weight=2)
+        root.rowconfigure(1, weight=7)
+        root.rowconfigure(2, weight=2)
+        root.rowconfigure(3, weight=2)
+        root.rowconfigure(4, weight=1)
+        root.rowconfigure(5, weight=10)
 
         root.columnconfigure(0, weight=3)
         root.columnconfigure(1, weight=3)
         root.columnconfigure(2, weight=3)
-        root.columnconfigure(3, weight=3)
+        root.columnconfigure(3, weight=2)
         root.columnconfigure(4, weight=2)
+
 
         # LABEL app_name
         tk.Label(
-                root,
-                text="qounter"
-        ).grid(row=0, column=0, columnspan=5)
+            root,
+            text="qounter",
+            bg='lightcyan2'
+        ).grid(row=0, column=0, columnspan=6, ipady=5, sticky='ENW')
 
 
         # BUTTON close
         tk.Button(
-                root, 
-                text="close", 
-                width=25, 
-                height=3, 
-                command=root.destroy
-        ).grid(row=5, column=0, columnspan=6, sticky='EW')
+            root, 
+            text="X", 
+            command=root.destroy,
+            bg='lightcyan4',
+            fg='white'
+        ).grid(row=0, column=6, ipady=0, ipadx=0, sticky='ENW')
+
+
+        # LABEL file_path_label
+        tk.Label(
+            root,
+            text="selected CSV:",
+            bg='lightcyan3'
+        ).grid(row=1, column=1, columnspan=2, sticky='S')
+
+
+        # LABEL sum_label
+        tk.Label(
+            root,
+            text="sum:",
+            bg='lightcyan3'
+        ).grid(row=1, column=3, sticky='S')
+
+
+        # LABEL margin_label
+        tk.Label(
+            root,
+            text="margin:",
+            bg='lightcyan3'
+        ).grid(row=1, column=4, sticky='S')
 
 
         # BUTTON open_file
         tk.Button(
-                root, 
-                text="open file", 
-                width=15,
-                height=2, 
-                command=self.upload_file
-        ).grid(row=1, column=0)
+            root, 
+            text="open file", 
+            width=15,
+            height=2, 
+            command=self.upload_file,
+            bg='white'
+        ).grid(row=2, column=0)
 
 
         # LABEL file_path
         self.file_label = tk.Label(
-                root,
-                text="No file selected"
+            root,
+            text="No file selected",
+            bg='white'
         )
-        self.file_label.grid(row=1, column=1, columnspan=2, sticky='EW')
+        self.file_label.grid(row=2, column=1, columnspan=2, ipady=5, sticky='EW')
 
 
         # ENTRY sum to calculate
         self.key_entry = tk.Entry(
-                root,
-                width=10,
+            root,
+            width=6,
         )
-        self.key_entry.grid(row=1, column=3, sticky='E')
+        self.key_entry.grid(row=2, column=3, ipady=4)
 
 
         # ENTRY +/- margin (optional)
         self.margin_entry = tk.Entry(
-                root,
-                width=10,
+            root,
+            width=6,
         )
-        self.margin_entry.grid(row=1, column=4, sticky='E')
+        self.margin_entry.grid(row=2, column=4, ipady=4)
 
 
         # BUTTON run_calc
         self.button_run = tk.Button(
-                root, 
-                text="calculate", 
-                width=15, 
-                height=2, 
-                command=self.search
+            root, 
+            text="calculate", 
+            width=15, 
+            height=2, 
+            command=self.search,
+            bg='white'
         )
-        self.button_run.grid(row=2, column=0)
+        self.button_run.grid(row=3, column=0)
 
 
         # PROGRESS BAR
         self.progress = ttk.Progressbar(
-                root, 
-                orient='horizontal', 
-                mode='determinate'
+            root, 
+            orient='horizontal', 
+            mode='determinate'
         )
-        self.progress.grid(row=2, column=1, columnspan=3, sticky='EW')
+        self.progress.grid(row=3, column=1, columnspan=4, sticky='EW')
 
 
         # LABEL progress_percent
         self.progress_label = tk.Label(
-                root, 
-                text="",
+            root, 
+            text="",
+            width=6,
+            bg='lightcyan3'
         )
-        self.progress_label.grid(row=2, column=5, sticky='W')
+        self.progress_label.grid(row=3, column=5, sticky='W')
 
 
     def search(self):
