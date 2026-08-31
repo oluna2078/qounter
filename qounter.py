3b3fc2ca5c2fe6a60cfe9431b9290da3d1a359ce
oluna2078 <oluna2078@protonmail.com>
2026-08-31 15:56:46 +0200

    added margin feature to cli

--- qounter.py
+++ qounter.py
@@ -7,11 +7,18 @@ parser = ap.ArgumentParser(description="A tool for calculating known partial sum
 
 parser.add_argument("-f", "--file", nargs=1, metavar="path", type=str, 
                     help="path to CSV input data")
-parser.add_argument("-s", "--sum", nargs=1, metavar="num", type=int, 
+parser.add_argument("-s", "--sum", nargs=1, metavar="float", type=float, 
                     help="partial sum that needs to be calculated using the elements from the input data")
+parser.add_argument("-m", "--margin", nargs=1, metavar="float", type=float, 
+                    help="+/- margin the sum can be off by (optional)")
 
 args = parser.parse_args()
 
 sums = csv_handler.csv_to_lst(args.file[0])
 
-cf.rec_sum_search(sums, key=args.sum[0])
+print(args.margin)
+
+if args.margin != None:
+    cf.rec_sum_search(sums, key=args.sum[0], margin=args.margin[0])
+else:
+    cf.rec_sum_search(sums, key=args.sum[0])
